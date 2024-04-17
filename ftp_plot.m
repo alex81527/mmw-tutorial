299759a134f51831be0fe4f770778f6a2edff322
Wei-Han Chen (Alex) <alex81527@gmail.com>
2024-04-17 12:54:19 -0400

    fix all plotting scripts for FTP

--- ftp_plot.m
+++ ftp_plot.m
@@ -1,8 +1,13 @@
 clear; close all;
 path(path, './lib');
 path(path, './mat_files');
+path(path, './codebooks');
 path(path, './ftp_plot_scripts');
 run('./lib/cvx-w64/cvx/cvx_setup.m')
+
+run("./gen_plot_data_microbenchmark.m")
+run("./ftp_plot_scripts/ftp_gen_plot_data_snr_throughput.m")
+run("./ftp_gen_plot_data_5g_superres.m")
 
 
 run("./ftp_plot_scripts/fig1_beamtraining_overhead.m")
@@ -13,6 +18,6 @@ run("./ftp_plot_scripts/micro_benchmark_angle.m")
 run("./ftp_plot_scripts/micro_benchmark_gain.m")
 run("./ftp_plot_scripts/snr_perf.m")
 run("./ftp_plot_scripts/beamtraining_overhead.m")
-run("./ftp_plot_scripts/super_resolution.m")
+run("./ftp_plot_scripts/super_resolution_perf.m")
 run("./ftp_plot_scripts/ofdm_beam.m")
 run("./ftp_plot_scripts/ofdm_perf.m")
